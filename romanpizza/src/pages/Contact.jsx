21206864ddb5e677e878360ea966485bca8a28ca
numelb <numelb@gmail.com>
2023-09-08 17:52:44 -0500

    fix romove romani pizza

--- romanpizza/src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <div>
-      <h2>
-        <i className="bi bi-envelope-at-fill"></i>Contact
-      </h2>
-    </div>
-  );
-}
--- romanpizza/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-//import Login from "./Login";
-export default function Home() {
-  return (
-    <>
-      <h2>Home</h2>
-    </>
-  );
-}
